b18f4910b58290577095f1d7c4c21f1d65be3b1d
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-10 20:54:32 -0500

    State showOptions

--- components/Sidebar/Notebook.tsx
+++ components/Sidebar/Notebook.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgMoreAlt } from 'react-icons/cg'
 
 const Notebook = ({ title }: { title: string }) => {
+  const [showOptions, setShowOptions] = useState(false)
+
   return (
-    <div className="group inline-flex w-full rounded-md p-1 pl-2 hover:cursor-pointer hover:bg-stone-100">
+    <div className="group inline-flex w-full items-center justify-between rounded-md p-1 pl-2 hover:cursor-pointer hover:bg-stone-100">
       <span className="text-stone-800">{title}</span>
-      <CgMoreAlt />
+      <div>
+        <CgMoreAlt
+          onClick={() => setShowOptions(!showOptions)}
+          className="mr-1 hidden h-5 w-5 rounded text-stone-800 hover:bg-stone-200 group-hover:block"
+        />
+      </div>
     </div>
   )
 }
