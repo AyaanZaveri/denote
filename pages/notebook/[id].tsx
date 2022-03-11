37a8cb87af9e2c8ff81256f98e894c113fc29224
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-10 22:20:17 -0500

    Clean up

--- pages/notebook/[id].tsx
+++ pages/notebook/[id].tsx
@@ -35,39 +35,7 @@ const NotebookIndex = ({ notebookNote, notes, docSnap }: ssProps) => {
 }
 
 export async function getServerSideProps(context: any) {
-  const notebookRef = collection(db, 'notebooks')
-  const ref = await doc(notebookRef, context.params.id)
-  const notesRef = collection(ref, 'notes')
-	const notebookNote = await getDocs(notesRef)
-
-	notebookNote.forEach((doc) => {
-		// doc.data() is never undefined for query doc snapshots
-		console.log(doc.data());
-	});
-	
-
-  //   const notes = notesQ.docs
-  //     .map((doc) => ({
-  //       ...doc.data(),
-  //     }))
-  //     .map((message) => ({
-  //       ...message,
-  //       createdAt: new Date(message.createdAt.seconds * 1000),
-  //     }))
-
-  //   const getNote = await getDoc(ref)
-  //   const notebookNote = {
-  //     ...getNote.data(),
-  //     id: getNote.id,
-  //   }
-
-  return {
-    props: {
-      //   notebookNote: notebookNote,
-      //   notes: JSON.stringify(notes),
-      // docSnap: notes,
-    },
-  }
+  
 }
 
 export default NotebookIndex
