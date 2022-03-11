299ed4b8a7e0ff0b17eaa18520714da30e7933ad
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-11 16:09:07 -0500

    Notebook Info

--- pages/notebook/[id].tsx
+++ pages/notebook/[id].tsx
@@ -3,22 +3,23 @@ import {
   doc,
   getDoc,
   getDocs,
+  onSnapshot,
   orderBy,
   query,
 } from 'firebase/firestore'
 import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
 import Notes from '../../components/Notes'
 import Sidebar from '../../components/Sidebar'
 import { auth, db } from '../../firebase'
 
 interface ssProps {
-  notebookNote: string[]
-  notes: any
-  docSnap: any
+  notebookInfo: string
+  notes: string
 }
 
-const NotebookIndex = ({ notes }: ssProps) => {
+const NotebookIndex = ({ notes, notebookInfo }: ssProps) => {
   const [user] = useAuthState(auth)
 
   const notesList = JSON.parse(notes)
@@ -40,6 +41,8 @@ const NotebookIndex = ({ notes }: ssProps) => {
 export async function getServerSideProps(context: any) {
   // Structure: notebooks -> {id} -> notes -> {id} -> {title, markdown, tag}
 
+  // Getting Note Data
+
   const notebookRef = collection(db, `notebooks/${context.params.id}/notes`)
 
   const notebookNote = await getDocs(notebookRef)
@@ -55,9 +58,20 @@ export async function getServerSideProps(context: any) {
 
   console.log(notes)
 
+  // Getting Notebook Info
+
+  let notebookDetails: any = {}
+
+  onSnapshot(doc(db, 'notebooks', context.params.id), (doc) => {
+    notebookDetails = console.log(doc.data())
+  })
+
+  console.log(notebookDetails)
+
   return {
     props: {
       notes: JSON.stringify(notes),
+      notebookInfo: notebookDetails,
     },
   }
 }
