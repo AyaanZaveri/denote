ea8b40551251ee46ea698b93797b6ca77d22e4b5
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-12 15:21:48 -0500

    Moving a tag

--- components/Notes/Note.tsx
+++ components/Notes/Note.tsx
@@ -15,14 +15,7 @@ interface Props {
   notebookID: string
 }
 
-const Note = ({
-  id,
-  timestamp,
-  title,
-  tag,
-  markdown,
-  notebookID,
-}: Props) => {
+const Note = ({ id, timestamp, title, tag, markdown, notebookID }: Props) => {
   const seconds = timestamp?.seconds
 
   const convertTime = (unixTime: number) => {
@@ -38,20 +31,22 @@ const Note = ({
 
   return (
     <div>
-      <button onClick={removeDoc}>
-        <HiOutlineTrash className="h-4 w-4 rounded-full text-stone-800 transition delay-200 ease-in-out hover:text-rose-600" />
-      </button>
-      <a href={`/note/${id}`}>
-        <div className="relative cursor-pointer rounded-lg border border-stone-300 bg-stone-100 p-3 transition ease-in-out hover:border-stone-400">
-          <div className="flex flex-col">
-            <span className="font-semibold text-stone-800">{title}</span>
-            <span className="text-sm text-stone-500">
-              {convertTime(seconds ? seconds : 0)}
-            </span>
-          </div>
-          <span className="absolute top-0 right-0 m-1 text-[12px]"></span>
+      <div className="relative cursor-pointer rounded-lg border border-stone-300 bg-stone-100 p-3 transition ease-in-out hover:border-stone-400">
+        <div className="flex flex-col">
+          <a href={`/note/${id}`}>
+            <span className="font-semibold text-stone-800 hover:underline transition ease-in-out delay-200">{title}</span>
+          </a>
+          <span className="text-sm text-stone-500">
+            {convertTime(seconds ? seconds : 0)}
+          </span>
         </div>
-      </a>
+        <button
+          onClick={removeDoc}
+          className="absolute top-0 right-0 m-1.5 text-[12px]"
+        >
+          <HiOutlineTrash className="h-4 w-4 rounded-full text-stone-800 transition delay-200 ease-in-out hover:text-rose-600" />
+        </button>
+      </div>
     </div>
   )
 }
