d5b654088a132dd384446fb7bb53a0661b4d4b5b
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-11 20:40:49 -0500

    Props

--- components/Notes/Note.tsx
+++ components/Notes/Note.tsx
@@ -8,8 +8,9 @@ interface Props {
 
 const Note = ({ title, tag, markdown }: Props) => {
   return (
-    <div className='bg-stone-100 border p-4 rounded-lg'>
-      <span>{title}</span>
+    <div className='bg-stone-100 border-stone-300 hover:border-stone-400 border p-4 rounded-lg relative transition ease-in-out cursor-pointer'>
+      <span className='font-semibold text-stone-800'>{title}</span>
+      <span className='text-sm absolute bottom-0 right-0 m-1 mr-2'>{tag}</span>
     </div>
   )
 }
