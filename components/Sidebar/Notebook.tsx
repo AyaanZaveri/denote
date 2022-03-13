cd6cee05c4202d73ad2071864cd1600c79d9ebc5
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-12 21:04:56 -0500

    removeNotebook

--- components/Sidebar/Notebook.tsx
+++ components/Sidebar/Notebook.tsx
@@ -1,34 +1,22 @@
 import React, { useState } from 'react'
 import { CgMoreAlt } from 'react-icons/cg'
-import { Popover } from '@headlessui/react'
-import { TrashIcon } from '@heroicons/react/outline'
+import { doc, deleteDoc, collection } from "firebase/firestore";
+import { db } from '../../firebase';
 
 const Notebook = ({ id, title }: { id: string; title: string }) => {
-  const [showOptions, setShowOptions] = useState(false)
+
+  const removeNotebook = () => {
+    deleteDoc(doc(collection(db, `notebooks`), id))
+  }
+
 
   return (
-    <div className="group inline-flex w-full items-center justify-between rounded-md p-1 pl-2 hover:cursor-pointer hover:bg-gray-100 ">
+    <div className="relative inline-flex w-full items-center justify-between rounded-md p-1 pl-2 hover:cursor-pointer hover:bg-gray-100 ">
       <a key={id} href={`/notebook/${id}`}>
         <span className="text-gray-800">{title}</span>
       </a>
       <div>
-        <Popover className="z-20 flex items-center">
-          <Popover.Button>
-            <CgMoreAlt
-              onClick={() => setShowOptions(!showOptions)}
-              className="mr-1 h-5 w-5 rounded text-gray-600 hover:bg-gray-200 group-hover:block"
-            />
-          </Popover.Button>
-
-          <Popover.Panel className="absolute z-30 mb-16">
-            <div className="h-12 w-32 border bg-white">
-              <div className="inline-flex text-gray-800 hover:text-blue-500">
-                <TrashIcon className="h-5 w-5" />
-                Delete
-              </div>
-            </div>
-          </Popover.Panel>
-        </Popover>
+        <CgMoreAlt onClick={removeNotebook} className="mr-1 h-5 w-5 rounded text-gray-600 hover:bg-gray-200" />
       </div>
     </div>
   )
