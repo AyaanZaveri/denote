1095293a4bdb76f3165582b8b99b52cd0ba3de53
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-31 17:10:03 -0400

    Fixing spacing for tags

--- components/Sidebar/Notebook.tsx
+++ components/Sidebar/Notebook.tsx
@@ -15,18 +15,20 @@ const Notebook = ({ id, title }: { id: string; title: string }) => {
 
   return (
     <div
-      className={`relative inline-flex w-full items-center justify-between transition delay-200 ease-in-out rounded-md p-1 pl-2 hover:cursor-pointer ${
+      className={`flex w-full flex-col justify-between rounded-md p-1 pl-2 transition delay-200 ease-in-out hover:cursor-pointer ${
         id == queryID ? 'bg-gray-200' : 'hover:bg-gray-100'
       }`}
     >
-      <a key={id} href={`/notebook/${id}`}>
-        <span className="text-gray-800">{title}</span>
-      </a>
-      <div>
-        <CgMoreAlt
-          onClick={removeNotebook}
-          className="mr-1 h-5 w-5 rounded text-gray-600 hover:bg-gray-200 transition delay-200 ease-in-out"
-        />
+      <div className={`inline-flex items-center justify-between`}>
+        <a key={id} href={`/notebook/${id}`}>
+          <span className="text-gray-800">{title}</span>
+        </a>
+        <div>
+          <CgMoreAlt
+            onClick={removeNotebook}
+            className="mr-1 h-5 w-5 rounded text-gray-600 transition delay-200 ease-in-out hover:bg-gray-200"
+          />
+        </div>
       </div>
     </div>
   )
