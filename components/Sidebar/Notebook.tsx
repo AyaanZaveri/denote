c5c63bcb05b1148930fd81faaa5d22e5aee083de
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-31 17:16:27 -0400

    Restoring state

--- components/Sidebar/Notebook.tsx
+++ components/Sidebar/Notebook.tsx
@@ -15,20 +15,18 @@ const Notebook = ({ id, title }: { id: string; title: string }) => {
 
   return (
     <div
-      className={`flex w-full flex-col justify-between rounded-md p-1 pl-2 transition delay-200 ease-in-out hover:cursor-pointer ${
+      className={`relative inline-flex w-full items-center justify-between rounded-md p-1 pl-2 transition delay-200 ease-in-out hover:cursor-pointer ${
         id == queryID ? 'bg-gray-200' : 'hover:bg-gray-100'
       }`}
     >
-      <div className={`inline-flex items-center justify-between`}>
-        <a key={id} href={`/notebook/${id}`}>
-          <span className="text-gray-800">{title}</span>
-        </a>
-        <div>
-          <CgMoreAlt
-            onClick={removeNotebook}
-            className="mr-1 h-5 w-5 rounded text-gray-600 transition delay-200 ease-in-out hover:bg-gray-200"
-          />
-        </div>
+      <a key={id} href={`/notebook/${id}`}>
+        <span className="text-gray-800">{title}</span>
+      </a>
+      <div>
+        <CgMoreAlt
+          onClick={removeNotebook}
+          className="mr-1 h-5 w-5 rounded text-gray-600 transition delay-200 ease-in-out hover:bg-gray-200"
+        />
       </div>
     </div>
   )
